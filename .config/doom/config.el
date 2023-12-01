d08bb639f78021f7392a988b483f153c29aeedd9
Oleksii Kapula <kon2vpalto@gmail.com>
2023-12-01 20:15:17 +0200

    Fix fish indent issue

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -52,9 +52,8 @@
 (setq-default tab-width 2
               standard-indent 2)
 
-(add-hook! 'sh-mode-hook
-  (setq sh-basic-offset 2
-        fish-indent-offset 2))
+(after! fish-mode
+  (setq fish-indent-offset 2))
 
 (setq mouse-wheel-scroll-amount '(2 ((shift) . hscroll))
       mouse-wheel-progressive-speed nil
