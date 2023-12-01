8369c159608c14946bf0076e3c1e0ca9cd580faa
Oleksii Kapula <kon2vpalto@gmail.com>
2023-12-01 16:05:56 +0200

    Add optional credentials
    I don't need them, but whatever

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -7,8 +7,9 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-;; (setq user-full-name "John Doe"
-;;       user-mail-address "john@doe.com")
+
+(setq user-full-name "Oleksii Kapula"
+      user-mail-address "")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
