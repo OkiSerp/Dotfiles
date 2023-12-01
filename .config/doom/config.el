a055c299c57881148752de08fa21921ca0a9e313
Oleksii Kapula <kon2vpalto@gmail.com>
2023-12-01 16:39:42 +0200

    Make comments' style italic in programming mode

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -77,6 +77,9 @@
 
 (setq display-line-numbers-type nil)
 
+(add-hook! 'prog-mode-hook
+  (face-remap-add-relative 'font-lock-comment-face :slant 'italic))
+
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 
