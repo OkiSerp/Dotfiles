347c83cc09816597a0dbbb6a350610bb3eb34f64
Oleksii Kapula <kon2vpalto@gmail.com>
2023-12-01 17:12:32 +0200

    Hide cursor shape in dashboard

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -185,6 +185,9 @@
   (add-hook! '+doom-dashboard-functions :append
     (insert "\n" (+doom-dashboard--center +doom-dashboard--width "Pure Evil‼"))))
 
+(add-hook! '+doom-dashboard-mode-hook
+  (setq-local evil-normal-state-cursor '(hbar . 0)))
+
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
