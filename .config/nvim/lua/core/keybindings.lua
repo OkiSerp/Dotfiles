9d638455efc878d83109fe24748d79640cdd2bca
Oleksii Kapula <kon2vpalto@gmail.com>
2023-12-02 08:58:02 +0200

    Add buffer setup keybindings

--- .config/nvim/lua/core/keybindings.lua
+++ .config/nvim/lua/core/keybindings.lua
@@ -16,3 +16,11 @@ bind(nv, "<leader>qq", vim.cmd.xall, opts)
 bind(nv, "<leader>qQ", ":qall!<Cr>", opts)
 
 bind(nv, "<leader>oe", vim.cmd.Ex, opts)
+
+vim.opt.hidden = true
+
+bind(nvi, "<M-q>", vim.cmd.bdelete, opts)
+bind(nvi, "<M-s>", vim.cmd.write, { noremap = true })
+
+bind(nvi, "<M-j>", vim.cmd.bnext, opts)
+bind(nvi, "<M-k>", vim.cmd.bprevious, opts)
