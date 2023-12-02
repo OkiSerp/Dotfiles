1163956c1bc63441f18e3df479fc62aed5cf71d8
Oleksii Kapula <kon2vpalto@gmail.com>
2023-12-02 09:17:04 +0200

    Configure catppuccin theme plugin

---
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,64 @@
+local plugin = {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  lazy = false,
+  priority = 999,
+}
+
+local opts = {
+  flavour = "mocha",
+  background = {
+    light = "latte",
+    dark = "mocha",
+  },
+  transparent_background = true,
+  show_end_of_buffer = true,
+  term_colors = false,
+  dim_inactive = {
+    enabled = false,
+    shade = "dark",
+    percentage = 0.15,
+  },
+  no_italic = false,
+  no_bold = false,
+  no_underline = false,
+  styles = {
+    comments = {},
+    conditionals = {},
+    loops = {},
+    functions = {},
+    keywords = {},
+    strings = {},
+    variables = {},
+    numbers = {},
+    booleans = {},
+    properties = {},
+    types = {},
+    operators = {},
+  },
+  color_overrides = {},
+  custom_highlights = {},
+  integrations = {
+    which_key = true,
+    telescope = {
+      enabled = true,
+    },
+    treesitter = true,
+    indent_blankline = {
+      enabled = true,
+      scope_color = "lavender",
+      colored_indent_levels = false,
+    },
+    noice = true,
+    mini = {
+      enabled = true,
+    },
+  },
+}
+
+plugin.config = function()
+  require("catppuccin").setup(opts)
+  vim.cmd.colorscheme "catppuccin"
+end
+
+return plugin
