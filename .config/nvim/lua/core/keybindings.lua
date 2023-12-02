006c95e7836e7de817e32bbf36230a89ac982e35
Oleksii Kapula <kon2vpalto@gmail.com>
2023-12-02 08:56:18 +0200

    Set aliases to simplify maps configuration

--- .config/nvim/lua/core/keybindings.lua
+++ .config/nvim/lua/core/keybindings.lua
@@ -1,2 +1,13 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " m"
+
+local n, v = "n", "v"
+local c, i = "c", "i"
+
+local nv = { "n", "v" }
+local nvi = { "n", "v", "i" }
+
+local silent = { silent = true }
+local opts = { noremap = true, silent = true }
+
+local bind = vim.keymap.set
