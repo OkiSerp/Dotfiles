9751c9f4294a14bedbe3f86d44ef69dae0ff4cb3
Oleksii Kapula <kon2vpalto@gmail.com>
2023-12-06 19:27:57 +0200

    Configure vim sneak plugin in one place

--- .config/nvim/after/plugin/vimsneak.lua
+++
@@ -1,7 +0,0 @@
-local _, ok = pcall(require, "vim-sneak")
-
-if not ok then
-  return
-end
-
-vim.cmd("let g:sneak#use_ic_scs = 1")
--- .config/nvim/lua/plugins/vimsneak.lua
+++ .config/nvim/lua/plugins/vimsneak.lua
@@ -1,4 +1,7 @@
 return {
   "justinmk/vim-sneak",
   event = { "BufReadPre", "BufNewFile" },
+  init = function()
+    vim.cmd("let g:sneak#use_ic_scs = 1")
+  end,
 }
