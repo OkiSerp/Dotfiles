31075daa4ed261725d1a67f84ca13a4ccf805a32
Oleksii Kapula <kon2vpalto@gmail.com>
2023-12-12 14:10:59 +0200

    Inherit doom-font's size for serif & sans

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -28,14 +28,13 @@ Modified version of `doom/delete-frame-with-prompt'."
     (setq doom-font (font-spec :family font :size 18)
           doom-big-font (font-spec :family font :size 24))))
 
-(let* ((family "Noto Sans")
-       (font (font-spec :family family :size 18)))
-  (when (doom-font-exists-p family)
-    (setq doom-variable-pitch-font font)))
+(let* ((font "Noto Sans"))
+  (when (doom-font-exists-p font)
+    (setq doom-variable-pitch-font (font-spec :family font))))
 
 (let* ((font "Noto Serif"))
   (when (doom-font-exists-p font)
-    (setq doom-serif-font (font-spec :family font :size 18))))
+    (setq doom-serif-font (font-spec :family font))))
 
 (use-package! catppuccin-theme
   :config
