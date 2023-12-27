e892f92f97d8b54fded5a15486c50dd8f9aaeaec
Oleksii Kapula <kon2vpalto@gmail.com>
2023-12-27 20:53:54 +0200

    Change fish configuration a bit

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,30 +6,36 @@ set -g fish_greeting
 
 fish_vi_key_bindings
 
-bind -M insert \el "forward-char"
-bind -M insert \ek "history-search-backward"
-bind -M insert \ej "history-search-forward"
-
 bind -M default \ek "history-search-backward"
 bind -M default \ej "history-search-forward"
 
-alias vi  (which nvim)
+bind -M insert \ek "history-search-backward"
+bind -M insert \ej "history-search-forward"
+
+bind -M insert \el "forward-char"
+
+alias vi (which nvim)
+alias vim (which nvim)
+
 set -gx EDITOR (which nvim)
 
 fish_add_path $HOME/.config/emacs/bin
 set -gx DOOMDIR $HOME/.config/doom
 
-fish_add_path $HOME/.local/bin
-
 alias mv "$(which mv) -i"
 alias cp "$(which cp) -i"
+
+alias df="$(which df) -h"
+alias free="$(which free) -m"
 
 alias neofetch "echo && $(which neofetch)"
 
 alias ls "lsd --group-dirs first --icon never --color always"
 alias la "lsd --group-dirs first --icon never --color always -A"
+
 alias ll "lsd --group-dirs first --icon always --icon-theme fancy --color always \
   --blocks permission,user,size,git,name -lA"
+
 alias lt "lsd --group-dirs first --icon always --icon-theme fancy --color always \
   --blocks permission,user,size,git,name -lAX --tree -I .git -I node_modules"
 
@@ -37,15 +43,12 @@ if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
 
 fundle plugin "IlanCosman/tide@v6"
 fundle plugin "PatrickF1/fzf.fish"
-fundle plugin "jorgebucaran/nvm.fish"
 
 fundle init
 
 set -gx FZF_DEFAULT_OPTS --bind=alt-j:down,alt-k:up
-set fzf_fd_opts --hidden --no-ignore --exclude=.git --exclude=node_modules
 
-set -U nvm_default_version lts
-set -U nvm_default_packages yarn pnpm
+set fzf_fd_opts --hidden --no-ignore --exclude=.git --exclude=node_modules
 
 if [ "$fish_key_bindings" = fish_vi_key_bindings ]
   bind -Minsert ! __history_previous_command
