7c3754b3e9995e1c15e662200ee099ff82ded045
Oleksii Kapula <kon2vpalto@gmail.com>
2024-03-24 12:34:28 +0200

    Back to 2 spaces

--- .config/nvim/lua/core/opts.lua
+++ .config/nvim/lua/core/opts.lua
@@ -17,9 +17,9 @@ vim.opt.guicursor = ""
 vim.opt.signcolumn = "yes"
 vim.opt.cursorline = true
 
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
 
 vim.opt.smartindent = true
 vim.opt.autoindent = true
