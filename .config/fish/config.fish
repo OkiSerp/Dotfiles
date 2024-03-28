7ab11e6ea20403336741257bab7ad01fdce41097
Oleksii Kapula <kon2vpalto@gmail.com>
2024-03-28 20:07:33 +0200

    Add theme lines for fzf command

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -28,7 +28,11 @@ fundle plugin "PatrickF1/fzf.fish"
 
 fundle init
 
-set -gx FZF_DEFAULT_OPTS --bind=alt-j:down,alt-k:up
+set -Ux FZF_DEFAULT_OPTS "--bind=alt-j:down,alt-k:up \
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+
 set fzf_fd_opts --hidden --no-ignore --exclude=.git --exclude=node_modules
 
 if [ "$fish_key_bindings" = fish_vi_key_bindings ]
