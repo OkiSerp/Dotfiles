0c9daa795367ffe7b17cf2c471924f39dac5440a
Oleksii Kapula <kon2vpalto@gmail.com>
2024-03-28 20:09:38 +0200

    Make fzf opts global, not universal

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -28,7 +28,7 @@ fundle plugin "PatrickF1/fzf.fish"
 
 fundle init
 
-set -Ux FZF_DEFAULT_OPTS "--bind=alt-j:down,alt-k:up \
+set -gx FZF_DEFAULT_OPTS "--bind=alt-j:down,alt-k:up \
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
