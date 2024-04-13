5eedfd6b66231db2f166f8c0f230f47a69acaa91
Oleksii Kapula <kon2vpalto@gmail.com>
2024-04-13 12:02:07 +0300

    Auto-remove trailing spaces & save cursor position

--- .config/nvim/lua/core/opts.lua
+++ .config/nvim/lua/core/opts.lua
@@ -2,7 +2,11 @@ vim.cmd("set shell=fish")
 
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = { "*" },
-  command = [[%s/\s\+$//e]],
+  callback = function()
+    local save_cursor = vim.fn.getpos(".")
+    pcall(function() vim.cmd [[%s/\s\+$//e]] end)
+    vim.fn.setpos(".", save_cursor)
+  end,
 })
 
 vim.g.mapleader = " "
