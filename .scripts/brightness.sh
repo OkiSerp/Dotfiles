531524a3aa578e1343bffed21e2c91cfea01c449
Oleksii Kapula <kon2vpalto@gmail.com>
2024-05-01 17:07:02 +0300

    Remove unneeded echo

--- .scripts/brightness.sh
+++ .scripts/brightness.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/env sh
 
-basedir="/sys/class/backlight/$(ls /sys/class/backlight/)/"
+basedir="/sys/class/backlight/intel_backlight/"
 
 old_brightness="$(cat ${basedir}'brightness')"
 
 max_brightness="$(cat ${basedir}'max_brightness')"
 
-# get current brightness %
 old_brightness_p="$(( 100 * ${old_brightness} / ${max_brightness} ))"
-echo ${old_brightness_p}
 
-# calculate new brightness %
 new_brightness_p="$(( ${old_brightness_p} $1 ))"
 
-# calculate new brightness value
 new_brightness="$(( ${max_brightness} * ${new_brightness_p} / 100 ))"
 
-# set the new brightness value
 sudo chmod 666 "${basedir}brightness"
 echo ${new_brightness} > "${basedir}brightness"
