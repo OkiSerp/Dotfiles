cf4862df5a265547dbddd255b10d0e80eade0525
Oleksii Kapula <kon2vpalto@gmail.com>
2024-05-15 18:27:06 +0300

    Delete suffix in script files

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env sh
 
 while true; do
-  vol="$(~/.scripts/volumestatus.sh)"
-  bat="$(~/.scripts/battery.sh)"
-  brt="󰃟 $(( 100 * "$(brightnessctl get)" / "$(brightnessctl max)" ))%"
-  sbar="${vol} | ${bat} | ${brt} | 󱑎 $(date '+%R')"
-  xsetroot -name "${sbar}"
-  sleep 1s
+  volume="$(~/.dotfiles/scripts/volume --status)"
+  battery="$(~/.dotfiles/scripts/battery)"
+  brightness="$(fish -c '~/.dotfiles/scripts/brightness --status')"
+  datetime="$(date '+%b %d %a') $(date '+%R')"
+
+  xsetroot -name "$volume / $battery / $brightness / $datetime"
+
+  sleep 700ms
 done &
 
 xrandr --size 1920x1080 &
@@ -15,15 +17,10 @@ xset r rate 350 55 &
 
 xinput set-prop "Synaptics TM3336-001" "libinput Tapping Enabled" 1 &
 
-xrdb -merge ~/.Xresources
-
 xbanish &
 
-if [[ -f "${HOME}/.config/sxhkd/sxhkdrc" ]]; then
-  sxhkd -c "${HOME}/.config/sxhkd/sxhkdrc"  &
-fi
+keysfile="$HOME/.config/sxhkd/sxhkdrc"
+[[ -e $keysfile ]] && sxhkd -c $keysfile &
 
-wall="${HOME}/.local/share/dwm/wallpaper"
-if [[ -f ${wall} ]]; then
-  feh --no-fehbg --bg-scale ${wall} &
-fi
+wallpaper="$HOME/.local/share/dwm/wallpaper"
+[[ -e $wallpaper ]] && feh --no-fehbg --bg-scale $wallpaper &
