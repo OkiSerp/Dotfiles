302b1b410f1859385c5689c23e0be675d154aa69
Oleksii Kapula <kon2vpalto@gmail.com>
2024-05-22 14:07:49 +0300

    Add alternative wallpaper

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -8,17 +8,15 @@ xset r rate 350 55 &
 
 xset s off -dpms &
 
-xinput set-prop "Synaptics TM3336-001" "libinput Tapping Enabled" 1 &
-
 xbanish &
 
+xinput set-prop "Synaptics TM3336-001" "libinput Tapping Enabled" 1 &
+
+setxkbmap -layout us,ua -option grp:shifts_toggle &
+
 sxhkdrc="$HOME/.config/sxhkd/sxhkdrc"
-wallpaper="$HOME/.local/share/dwm/wall.jpg"
+wall="$HOME/.local/share/dwm/wall.jpg"
 
 test -e "${sxhkdrc}" && sxhkd -c "${sxhkdrc}" &
 
-if [[ -e "${wallpaper}" ]];then
-  feh --no-fehbg --bg-scale "${wallpaper}" &
-fi
-
-setxkbmap -layout us,ua -option grp:shifts_toggle &
+test -e "${wall}" && feh --no-fehbg --bg-scale "${wall}" &
